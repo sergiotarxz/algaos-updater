9a7d96bf9068c6c02fdaed66c20983c764829d28
sergio <sergiotarxz@posteo.net>
2026-09-15 23:18:57 +0200

    Using sudo tee to create the binrepos reference

--- lib/AlgaOS/Updater/GUI.pm
+++ lib/AlgaOS/Updater/GUI.pm
@@ -280,6 +280,10 @@ sub _update($self) {
         system qw{rm -v /etc/portage/binrepos.conf/gentoo.conf};
         {
             open my $bin_fh, '>', '/etc/portage/binrepos.conf/algaos.conf';
+            open my $bin_fh, '|-',
+              qw{sudo tee /etc/portage/binrepos.conf/algaos.conf}
+              or die "open: $!";
+
             say $bin_fh <<"EOF";
 [algaos]
 
@@ -288,6 +292,8 @@ sync-uri = https://algaos.com/dist/@{[$self->_machine_id]}/binpkg
 priority = 1
 verify-signature = false
 EOF
+
+            close $bin_fh or die "tee: $?";
         }
         if ( system qw{sudo emerge --noreplace --getbinpkg @world @system} ) {
             exit 1;
