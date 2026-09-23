f7b66ffa38ad6fc0690488e300f933f84e145204
Sergio <sergiotarxz@posteo.net>
2026-09-24 01:49:48 +0200

    Better -preference

--- lib/AlgaOS/Updater/GUI.pm
+++ lib/AlgaOS/Updater/GUI.pm
@@ -164,12 +164,12 @@ sub is_there_updates($self) {
           qw{sudo tee /etc/portage/binrepos.conf/algaos.conf}
           or die "open: $!";
 
-        my $preference = $self->_channel_preference // '';
+        my $preference = '-'.$self->_channel_preference // '';
         say $bin_fh <<"EOF";
 [algaos]
 
-location = https://algaos.com/dist/@{[$self->_machine_id]}/binpkg/$preference
-sync-uri = https://algaos.com/dist/@{[$self->_machine_id]}/binpkg/$preference
+location = https://algaos.com/dist/@{[$self->_machine_id]}/binpkg-$preference
+sync-uri = https://algaos.com/dist/@{[$self->_machine_id]}/binpkg-$preference
 priority = 1
 verify-signature = false
 EOF
